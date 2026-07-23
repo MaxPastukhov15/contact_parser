070c477e679ab33c6b149b33daf4ee8afc68f69b
MaxPastukhov15 <st131119@student.spbu.ru>
2026-07-23 19:21:03 +0300

    fix: fix run_scraper

--- src/cli/run_scraper_cmd.py
+++ src/cli/run_scraper_cmd.py
@@ -34,7 +34,8 @@ def run_scraper_cmd(args: argparse.Namespace) -> None:
 
     import twisted.internet.asyncioreactor
 
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     twisted.internet.asyncioreactor.install()
 
     from scrapy.crawler import CrawlerProcess
--- src/companies_website/run_scraper.py
+++ src/companies_website/run_scraper.py
@@ -21,7 +21,8 @@ def main() -> None:
 
     import twisted.internet.asyncioreactor
 
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     twisted.internet.asyncioreactor.install()
 
     from scrapy.crawler import CrawlerProcess
